81114795a98fc02ed5fce5d2d4f985ba4d2b97f3
Michael Cordingley <Michael.Cordingley@gmail.com>
2019-08-29 21:57:12 -0400

    Create library facade.

---
+++ src/Facade.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace MCordingley\LaraLock;
+
+use Exception;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Contracts\Encryption\DecryptException;
+use Illuminate\Contracts\Hashing\Hasher;
+
+final class Facade
+{
+    private $encrypter;
+    private $hasher;
+
+    public function __construct(AeadEncrypter $encrypter, Hasher $hasher)
+    {
+        $this->encrypter = $encrypter;
+        $this->hasher = $hasher;
+    }
+
+    public function check(string $value, Authenticatable $user, array $options = []): bool
+    {
+        try {
+            return $this->hasher->check($value, $this->decrypt($user->getAuthPassword(), $user->getAuthIdentifier(), false), $options);
+        } catch (DecryptException $exception) {
+            return false;
+        }
+    }
+
+    /**
+     * @param string $value
+     * @param Authenticatable $user
+     * @param array $options
+     * @return string
+     * @throws Exception
+     */
+    public function hash(string $value, Authenticatable $user, array $options = []): string
+    {
+        $identifier = $user->getAuthIdentifier();
+
+        if ($identifier === null) {
+            throw new Exception('No identifier set on provided user.');
+        }
+
+        return $this->encrypt($this->hasher->make($value, $options), $identifier, false);
+    }
+
+    public function needsRehash(string $value, Authenticatable $user, array $options = []): bool
+    {
+        try {
+            return $this->hasher->needsRehash($this->decrypt($value, $user->getAuthIdentifier(), false), $options);
+        } catch (DecryptException $exception) {
+            return true;
+        }
+    }
+
+    public function decrypt(string $value, string $additionalData, bool $unserialize = true): string
+    {
+        return $this->encrypter->decrypt($value, $additionalData, $unserialize);
+    }
+
+    public function encrypt($value, string $additionalData, bool $serialize = true): string
+    {
+        return $this->encrypter->encrypt($value, $additionalData, $serialize);
+    }
+}
