0807881572e2d5267783ce890e4cb24cc74337f0
Michael Cordingley <Michael.Cordingley@gmail.com>
2019-08-29 21:57:38 -0400

    Create Laravel style facade to point to the actual facade class.

---
+++ src/LaraLock.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace MCordingley\LaraLock;
+
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * @method static bool check(string $value, Authenticatable $user, array $options = [])
+ * @method static string hash(string $value, Authenticatable $user, array $options = [])
+ * @method static bool needsRehash(string $value, Authenticatable $user, array $options = [])
+ * @method static string decrypt(string $value, string $additionalData, bool $unserialize = true)
+ * @method static string encrypt($value, string $additionalData, bool $serialize = true)
+ *
+ * @see \MCordingley\LaraLock\Facade
+ */
+final class LaraLock extends Facade
+{
+    public static function getFacadeAccessor()
+    {
+        return 'laralock';
+    }
+}
