a2fb0fd2cfd5d6aea1780cf912ffd89ec580e579
Michael Cordingley <Michael.Cordingley@gmail.com>
2019-08-30 23:30:33 -0400

    Test binding of user provider.

--- tests/LaravelBindingsTest.php
+++ tests/LaravelBindingsTest.php
@@ -3,6 +3,8 @@
 namespace MCordingley\LaraLockTests;
 
 use Illuminate\Auth\GenericUser;
+use Illuminate\Support\Facades\Auth;
+use MCordingley\LaraLock\Auth\LockedUserProvider;
 use MCordingley\LaraLock\LaraLock;
 use MCordingley\LaraLock\ServiceProvider;
 use Orchestra\Testbench\TestCase;
@@ -24,5 +26,22 @@ final class LaravelBindingsTest extends TestCase
         $user->password =  LaraLock::make('foo', $user);
 
         static::assertTrue(LaraLock::check('foo', $user));
+    }
+
+    public function testProviderRegistration()
+    {
+        config([
+            'auth' => [
+                'providers' => [
+                    'users' => [
+                        'driver' => 'laralock',
+                        'model' => 'foo',
+                    ],
+                ],
+            ],
+            'laralock.key' => 'base64:/dgzsXQlhjIRRMIu9mt/IU3N54a8njxKK1zvAUYzzbI=',
+        ]);
+
+        static::assertInstanceOf(LockedUserProvider::class, Auth::createUserProvider('users'));
     }
 }
